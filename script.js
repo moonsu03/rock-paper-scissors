e3a7429323738dd846c28f566cc8ca9dc34f0402
Radmir Yamalitdinov <104298711+moonsuta@users.noreply.github.com>
2023-02-09 22:10:50 +0500

    Add "round of the game" function
    Basic Test (to examine the func) with Rock as player's selection
    is also added at the bottom of script.js file
    It'll be removed.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
+//randomly return rock, paper or scissors
 function getComputerChoice() {
-  //randomly return rock, paper or scissors
   let randomNum = Math.floor(Math.random() * 3);
   if (randomNum == 0) {
     return "Rock";
@@ -9,3 +9,52 @@ function getComputerChoice() {
     return "Scissors";
   }
 }
+
+//play a round of the game
+function playRound(playerSelection, computerSelection) {
+  /* convert player's string to uppercase first letter
+  to match with computer's string no matter what */
+  playerSelection = playerSelection.toLowerCase();
+  playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1);
+
+  //message in case the player wins
+  let winMessage = () =>
+    `You win! ${playerSelection} wins ${computerSelection}`;
+
+  //message in case the player loses
+  let loseMessage = () =>
+    `You lose! ${computerSelection} wins ${playerSelection}`;
+
+  //message in case it's tie
+  let tieMessage = () => `It's tie!`;
+
+  if (playerSelection == "Rock") {
+    if (computerSelection == "Paper") {
+      return loseMessage();
+    } else if (computerSelection == "Scissors") {
+      return winMessage();
+    } else {
+      return tieMessage();
+    }
+  } else if (playerSelection == "Paper") {
+    if (computerSelection == "Scissors") {
+      return loseMessage();
+    } else if (computerSelection == "Rock") {
+      return winMessage();
+    } else {
+      return tieMessage();
+    }
+  } else if (playerSelection == "Scissors") {
+    if (computerSelection == "Rock") {
+      return loseMessage();
+    } else if (computerSelection == "Paper") {
+      return winMessage();
+    } else {
+      return tieMessage();
+    }
+  }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
